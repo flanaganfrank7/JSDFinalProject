de3135f737cc68102ab0d13b332c90f4b9887649
Frank Flanagan <frankflanagan17@gmail.comgit config --global user.email frankflanagan17@gmail.com>
2018-05-16 23:23:37 -0400

    created data.js + reference.js. Consolidated app.js.Cleaned

---
+++ data.js
@@ -0,0 +1,23 @@
+let userData = {
+"user1": [
+	{
+	"id": 1,
+	"venueName":"Subway",
+	"cat":"QSR",
+	"la": 40.76727216,
+	"lo": -73.99392888,
+	"timestamp":"monday"
+	}]
+"user2": [
+	{
+	"id": 2,
+	"venueName":"TJ Maxx",
+	"cat":"Retail",
+	"la": 40.76727216,
+	"lo": -73.99392888,
+	"timestamp":"wednesday"
+	}]
+}
+
+
+// userData.user2[0].venueName WILL RETURN 'TJ Maxx'
